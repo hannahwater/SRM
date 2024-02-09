1805424d0a57d589268df2cee479190fea1fae62
hannahwater <144190851+hannahwater@users.noreply.github.com>
2024-02-09 12:49:33 +1100

    Delete untar_files_in_directory.R

--- untar_files_in_directory.R
+++
@@ -1,7 +0,0 @@
-untar_files_in_directory <- function(directory) {
-  tgz_files <- list.files(directory, pattern = "\\.tgz$", full.names = TRUE)
-  for (tgz_file in tgz_files) {
-    untar(tgz_file, exdir = directory)
-  }
-  return()
-}
