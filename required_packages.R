14505da60314197784ebad620dab04cebf866005
hannahwater <144190851+hannahwater@users.noreply.github.com>
2024-06-15 15:24:22 +1000

    Delete required_packages.R

--- required_packages.R
+++
@@ -1,9 +0,0 @@
-required_packages <- function() {
-  packages <- c('foreach', 'doParallel', 'RCurl', 'curl','sf','ncdf4','lubridate','airGR','airGRteaching','rtop','dplyr')
-  for (pkg in packages) {
-    if (!require(pkg, character.only = TRUE)) {
-      install.packages(pkg)
-      library(pkg, character.only = TRUE)
-    }
-  }
-}
