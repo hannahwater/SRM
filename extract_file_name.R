5f543add2367f8d067eb9e1859ade7cb77da3c8d
hannahwater <144190851+hannahwater@users.noreply.github.com>
2024-06-15 14:23:14 +1000

    Delete extract_file_name.R

--- extract_file_name.R
+++
@@ -1,9 +0,0 @@
-extract_file_name <- function(ftp_line) {
-  parts <- strsplit(ftp_line, " ")[[1]]
-  parts <- parts[nzchar(parts)]  # Remove empty strings
-  if (length(parts) >= 9) {
-    return(paste(parts[9:length(parts)], collapse = " "))
-  } else {
-    return(NULL)
-  }
-}
